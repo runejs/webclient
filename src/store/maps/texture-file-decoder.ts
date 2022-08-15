95e6ee4e4a61d6a52dd23f551c1b1e2824922465
James Monger <jameskmonger@hotmail.co.uk>
2022-08-15 18:39:58 +0100

    feat: add resizeToLibSize

--- src/store/maps/texture-file-decoder.ts
+++ src/store/maps/texture-file-decoder.ts
@@ -1,7 +1,42 @@
 import { ByteBuffer } from "@runejs/common";
+import { adjustBrightness } from "../../common/color/brightness";
 import { SpriteState } from "../sprites/sprite-transcoder";
 import { store } from "../store";
 import { Texture, TEXTURE_INTENSITY, TEXTURE_SIZE } from "./texture";
+
+function resizeToLibSize(s: SpriteState) {
+    const imgWidth = s.width;
+    const imgHeight = s.height;
+    const maxWidth = TEXTURE_SIZE;
+    const maxHeight = TEXTURE_SIZE;
+
+    // TODO ?
+    const xDrawOffset = 0;
+    const yDrawOffset = 0;
+
+    if (imgWidth === maxWidth && imgHeight === maxHeight) {
+        return s;
+    }
+
+    const resizedPixels = new Array<number>(maxWidth * maxHeight);
+    let pixelCount = 0;
+    for (let y = 0; y < imgHeight; y++) {
+        for (let x = 0; x < imgWidth; x++) {
+            resizedPixels[x + xDrawOffset + (y + yDrawOffset) * maxWidth] =
+                s.pixels[pixelCount++];
+        }
+    }
+
+    const newSprite: SpriteState = {
+        ...s,
+
+        width: maxWidth,
+        height: maxHeight,
+        pixels: resizedPixels,
+    };
+
+    return newSprite;
+}
 
 export class TextureFileDecoder {
     static readonly textureFiles = new Map<number, Texture>();
@@ -45,7 +80,7 @@ export class TextureFileDecoder {
             for (let x = 0; x < spriteCount - 1; x++) {
                 // junk?
                 const junk = textureData.get("byte", "unsigned");
-                console.log({ junk })
+                console.log({ junk });
             }
         }
 
@@ -60,7 +95,16 @@ export class TextureFileDecoder {
 
         console.log({ direction, speed });
 
-        const texture = new Texture(textureId, rgb, opaque, spriteIds, renderTypes, colors, direction, speed);
+        const texture = new Texture(
+            textureId,
+            rgb,
+            opaque,
+            spriteIds,
+            renderTypes,
+            colors,
+            direction,
+            speed
+        );
 
         TextureFileDecoder.textureFiles.set(textureId, texture);
 
@@ -84,14 +128,12 @@ export class TextureFileDecoder {
         texture.pixels = new Array<number>(colorCount * 4);
 
         for (let i = 0; i < texture.spriteIds.length; i++) {
-            const sprite = sprites[i];
-
-            // TODO ? resizeToLibSize?
+            const sprite = resizeToLibSize(sprites[i]);
 
             const spritePixels = sprite.pixelIdx;
             const spritePalette = sprite.palette;
             const color = texture.colors[i];
-            
+
             if ((color & ~0xffffff) === 0x3000000) {
                 const non_green = color & 0xff00ff;
                 const g = (color >> 8) & 0xff;
@@ -99,16 +141,20 @@ export class TextureFileDecoder {
                     let palette = spritePalette[p];
                     if ((palette & 0xffff) === palette >> 8) {
                         palette &= 0xff;
-                        spritePalette[p] = non_green * palette >> 8 & 0xff00ff | g * palette & 0xff00;
+                        spritePalette[p] =
+                            (((non_green * palette) >> 8) & 0xff00ff) |
+                            ((g * palette) & 0xff00);
                     }
                 }
             }
 
             for (let p = 0; p < spritePalette.length; p++) {
-                spritePalette[p] = adjustBrightness(spritePalette[p], TEXTURE_INTENSITY);
+                spritePalette[p] = adjustBrightness(
+                    spritePalette[p],
+                    TEXTURE_INTENSITY
+                );
             }
 
-            
             let renderType;
             if (i === 0) {
                 renderType = 0;
@@ -119,20 +165,29 @@ export class TextureFileDecoder {
             if (renderType === 0) {
                 if (sprite.width === size) {
                     for (let p = 0; p < colorCount; p++) {
-                        texture.pixels[p] = spritePalette[spritePixels[p] & 0xff];
+                        texture.pixels[p] =
+                            spritePalette[spritePixels[p] & 0xff];
                     }
                 } else if (sprite.width === 64 && size === 128) {
                     let index = 0;
                     for (let y = 0; y < size; y++) {
                         for (let x = 0; x < size; x++) {
-                            texture.pixels[index++] = spritePalette[spritePixels[(x >> 1) + ((y >> 1) << 6)] & 0xff];
+                            texture.pixels[index++] =
+                                spritePalette[
+                                    spritePixels[(x >> 1) + ((y >> 1) << 6)] &
+                                        0xff
+                                ];
                         }
                     }
                 } else if (sprite.width === 128 && size === 64) {
                     let index = 0;
                     for (let y = 0; y < size; y++) {
                         for (let x = 0; x < size; x++) {
-                            texture.pixels[index++] = spritePalette[spritePixels[(x << 1) + ((y << 1) << 7)] & 0xff];
+                            texture.pixels[index++] =
+                                spritePalette[
+                                    spritePixels[(x << 1) + ((y << 1) << 7)] &
+                                        0xff
+                                ];
                         }
                     }
                 } else {
@@ -143,24 +198,13 @@ export class TextureFileDecoder {
 
         for (let i = 0; i < colorCount; i++) {
             texture.pixels[i] &= 0xf8f8ff;
-            
+
             const pixel = texture.pixels[i];
-            texture.pixels[i + colorCount] = pixel - (pixel >>> 3) & 0xf8f8ff;
-            texture.pixels[i + colorCount + colorCount] = pixel - (pixel >>> 2) & 0xf8f8ff;
-            texture.pixels[i + colorCount + colorCount + colorCount] = pixel - (pixel >>> 2) - (pixel >>> 3) & 0xf8f8ff;
+            texture.pixels[i + colorCount] = (pixel - (pixel >>> 3)) & 0xf8f8ff;
+            texture.pixels[i + colorCount + colorCount] =
+                (pixel - (pixel >>> 2)) & 0xf8f8ff;
+            texture.pixels[i + colorCount + colorCount + colorCount] =
+                (pixel - (pixel >>> 2) - (pixel >>> 3)) & 0xf8f8ff;
         }
     }
-}
-
-function adjustBrightness(color: number, intensity: number) {
-    let r =  (color >> 16) / 256.0;
-    let g =  ((color >> 8) & 0xff) / 256.0;
-    let b =  (color & 0xff) / 256.0;
-    r = Math.pow(r, intensity);
-    g = Math.pow(g, intensity);
-    b = Math.pow(b, intensity);
-    const outR = (r * 256.0) | 0;
-    const outB = (g * 256.0) | 0;
-    const outC = (b * 256.0) | 0;
-    return (outR << 16) + (outB << 8) + outC;
 }
